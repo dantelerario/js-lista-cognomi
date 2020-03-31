1cb0d708134be23fe7bf209260839bfa4a3ee9fc
Dante L'Erario <dantelerario@gmail.com>
2020-03-31 16:21:49 +0200

    JS list creation done

---
+++ js/master.js
@@ -0,0 +1,22 @@
+// chiedere all’utente il cognome
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione della lista in cui  il nuovo utente si trova
+
+var userSurname = prompt('Insert your surname');
+var listSurname = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+
+var list = document.getElementById('ulList');
+
+listSurname.push(userSurname);
+console.log(listSurname);
+
+for (var i = 0; i < listSurname.length; i++) {
+    var surname = listSurname[i];
+
+    var lastSurname = list.innerHTML;
+
+    var newlistSurname = '<li>' + surname + '</li>';
+    list.innerHTML = lastSurname + newlistSurname;
+
+}
